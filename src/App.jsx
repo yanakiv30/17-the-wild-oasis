574fc3cefa2a8ebf0b4368c8383706ba7c2c42f5
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-04-11 07:44:27 +0300

    props.size

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button  from "./ui/Button";
-import Input  from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -19,11 +15,34 @@ export default function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The wild oasis</H1>
-        <Button onClick={() => alert("Check in")}>Check in</Button>
-        <Button onClick={() => alert("Check out")}>Check out</Button>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
+        <Row type="horizontal">
+          <Heading as="h1">The wild oasis</Heading>
+          <div>
+            <Heading as="h2">Check in and out</Heading>
+            <Button
+              variation="primary"
+              size="medium"
+              onClick={() => alert("Check in")}
+            >
+              Check in
+            </Button>
+            <Button
+              variation="secondary"
+              size="small"
+              onClick={() => alert("Check out")}
+            >
+              Check out
+            </Button>
+          </div>
+        </Row>
+
+        <Row>
+          <Heading as="h3">Form</Heading>
+          <form>
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </form>
+        </Row>
       </StyledApp>
     </>
   );
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -48,19 +48,9 @@ const variations = {
   `,
 };
 
- const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
+const Button = styled.button`
   border: none;
   border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
   box-shadow: var(--shadow-sm);
-  cursor: pointer;
-&:hover {
-  background-color: var(--color-brand-800 )
-}
-
 `;
- export default Button;
+export default Button;
