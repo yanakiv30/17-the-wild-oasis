dbeabc17971ba70cfd8a572578f1e601833bce7f
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-04-12 12:33:34 +0300

    var grey

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -6,10 +6,11 @@ import styled from "styled-components";
 const StyledAppLayout = styled.div`
     display: grid;
     grid-template-columns: 26rem 1fr;
+    grid-template-rows: auto 1fr;
     height: 100vh;
 `
 const Main = styled.main`
-    background-color: green;
+    background-color:var(--color-grey-50);
     padding: 4rem 4.8rem 6.4rem;
 `;
 
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 const StyledSidebar= styled.aside`
-    background-color: blue;
+    background-color: var(--color-grey-0);
     padding: 3.2rem 2.4rem;
     border: 1px solid var(--color-grey-100);
+    grid-row: 1 / -1;
 `
 
 export default function Sidebar() {
