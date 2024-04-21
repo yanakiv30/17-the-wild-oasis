dee3db4b24e905b6624516c2d52bdae819a664c7
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-04-21 11:36:37 +0300

    Using getCabins() in Cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,1 +1,10 @@
-export function getCabins() {};
+import supabase from "./supabase";
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabins could not be loaded");
+  }
+  return data;
+}
