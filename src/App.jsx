87b4477dc479f1e35386cef3fcad95db81ddb50b
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-04-22 20:45:58 +0300

    ReactQueryDevtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import DashBoard from "./pages/DashBoard";
@@ -11,12 +11,20 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    query: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false}/>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -34,6 +42,6 @@ export default function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -12,6 +12,7 @@ useEffect(function(){
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://vqzzeqlvvzgqzkflzduw.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" />
     </Row>
   );
 }
