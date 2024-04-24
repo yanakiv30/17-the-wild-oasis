161ba0f51553d91f4f1dee07ed34447c5f506b6f
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-04-24 08:04:08 +0300

    deleteCabin002

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -9,4 +9,15 @@ export async function getCabins() {
   return data;
 }
 
-export async function deleteCabin() {}
+export async function deleteCabin(id) {
+  const { data, error } = await supabase
+    .from('cabins')
+    .delete()
+    .eq('id', id)
+
+    if (error) {
+      console.log(error);
+      throw new Error("Cabin could not be deleted");
+    }
+    return data;
+  }
