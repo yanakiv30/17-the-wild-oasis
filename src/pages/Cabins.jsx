6b28bf222bd3aad5177b21272c08810c12a731d8
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-04-25 07:02:34 +0300

    showForm

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,8 +2,11 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import { useState } from "react";
 
 function Cabins() {
+const [showForm, setShowForm]= useState(false);
 
   return (
     <> 
@@ -14,6 +17,8 @@ function Cabins() {
 
     <Row>
       <CabinTable />
+
+      <Button>Add new Cabin</Button>
     </Row>
      </>
    
