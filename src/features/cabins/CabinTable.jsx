e5455fed660405da1e332e4d10d38dc848854f89
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-05-31 22:28:44 +0300

    render

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,11 +1,8 @@
-
 import styled from "styled-components";
-import Spinner from "../../ui/Spinner"
+import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
-import {useCabins} from "./useCabins"
+import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
-
-
 
 const TableHeader = styled.header`
   display: grid;
@@ -23,19 +20,26 @@ const TableHeader = styled.header`
 `;
 
 function CabinTable() {
-  const {isLoading, cabins} = useCabins();
-  if(isLoading) return <Spinner />
+  const { isLoading, cabins } = useCabins();
+  if (isLoading) return <Spinner />;
 
-  return <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
-    <Table.Header>
-      <div></div>
-      <div>Cabin </div>
-      <div>Capacity</div>
-      <div>Price</div>
-      <div>Discount</div>
-      <div></div>      
-    </Table.Header>
-    {cabins.map(cabin=> <CabinRow cabin={cabin} key={cabin.id} />)}
-  </Table>
+  return (
+    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+      <Table.Header>
+        <div></div>
+        <div>Cabin </div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </Table.Header>
+      <Table.Body
+        data={cabins}
+        render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+      />
+     
+      
+    </Table>
+  );
 }
 export default CabinTable;
