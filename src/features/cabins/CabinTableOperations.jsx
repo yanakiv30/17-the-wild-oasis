3e9614cd09646fad84c5b68776434e194f0b8e2f
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-06-05 18:54:30 +0300

    filtering-finish

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,8 +1,17 @@
-import TableOperations from "../../ui/TableOperations"
-import Filter from "../../ui/Filter"
+import TableOperations from "../../ui/TableOperations";
+import Filter from "../../ui/Filter";
 function CabinTableOperations() {
-    return <TableOperations>
-        <Filter></Filter>
+  return (
+    <TableOperations>
+      <Filter
+        filterField="discount"
+        options={[
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "No discount" },
+          { value: "with-discount", label: "With discount" }
+        ]}
+      />
     </TableOperations>
+  );
 }
-export default CabinTableOperations
+export default CabinTableOperations;
